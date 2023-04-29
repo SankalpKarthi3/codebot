03379222e0dc03398e4dddbe1a86af8b215bedf7
sankalpkarthi <93033499+SankalpKarthi3@users.noreply.github.com>
2023-04-29 06:02:32 +0530

    Update finaltest.py

--- finaltest.py
+++ finaltest.py
@@ -81,7 +81,7 @@ if api:
         st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k = k)
 
     #Template
-    template = """You are a environmental activist assistant to a human, powered by a large language model trained by OpenAI.
+    templates = {"Zero-Shot" : """You are a environmental activist assistant to a human, powered by a large language model trained by OpenAI.
 
 You are designed to be able to assist  related to climate change, environment and weather forecasting, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics related to climate chalenge and weather forecasting. As a language model, you are able to generate human-like text based on the input you receive, allowing you to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
 
@@ -108,12 +108,27 @@ Current conversation:
 {history}
 Last line:
 Human: {input}
-You:"""
+You:""", "Non-environmental" : """You are an assistant to a human, powered by a large language model trained by OpenAI.
+
+You are designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, you are able to generate human-like text based on the input you receive, allowing you to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+You are constantly learning and improving, and your capabilities are constantly evolving. You are able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. You have access to some personalized information provided by the human in the Context section below. Additionally, you are able to generate your own text based on the input you receive, allowing you to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, you are a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether the human needs help with a specific question or just wants to have a conversation about a particular topic, you are here to assist.
+
+Context:
+{entities}
+
+Current conversation:
+{history}
+Last line:
+Human: {input}
+You:"""}
 
     #Create the Conversation Chain
     Conversation = ConversationChain(
             llm = llm,
-            prompt = PromptTemplate(template = template, input_variables = ["entities", "history", "input"]),
+            prompt = PromptTemplate(template = templates[template], input_variables = ["entities", "history", "input"]),
             #prompt = ENTITY_MEMORY_CONVERSATION_TEMPLATE,
             memory = st.session_state.entity_memory
             )
